c7ccdb85ad4ef063d3eb91617994b2d4fc0e7950
xXKWanx <153375467+xXKWanx@users.noreply.github.com>
2026-02-12 09:42:23 +0100

    Mise a jour settings

---
+++ settings.py
@@ -0,0 +1,17 @@
+#---Configuration de l'écran :
+SCREEN_WIDTH = 1280 #Largeur
+SCREEN_HEIGHT = 720 #Hauteir
+FPS = 60 #Image par seconde
+TITLE = "SpaceK'tcher" #Titre du jeu
+
+#---Liste des couleurs :
+OFF_WHITE = (250, 250, 250) # Blanc cassé
+DEEP_DARK = (30, 30, 35) # Noir
+CHARCOAL  = (50, 55, 65) # Gris Foncé
+
+SOFT_RED    = (230, 90, 90)   # Rouge corail
+SOFT_GREEN  = (110, 210, 140) # Vert menthe
+SOFT_BLUE   = (100, 160, 240) # Bleu ciel agréable
+SOFT_YELLOW = (245, 230, 100) # Jaune moutarde clair
+SOFT_PURPLE = (180, 120, 220) # Violet lavande
+SOFT_CYAN   = (100, 220, 220) # Turquoise
