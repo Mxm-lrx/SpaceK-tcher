4a2fc171a8756b8a23c71ab18046cae0931d9d4c
jules <jules.trentler@gmail.com>
2026-03-18 10:53:41 +0100

    Modification utils.py : ajout fonction import_folder

--- utils.py
+++ utils.py
@@ -56,3 +56,21 @@ def load_texture(file_name, convert_alpha=True, scale_to=None, fallback_surface=
 def import_image(path: str) -> pygame.Surface:
     # convert_alpha() : optimise l'image pour Pygame et gère la transparence
     return pygame.image.load(path).convert_alpha()
+
+def import_folder(path: str) -> List[pygame.Surface]:
+    surface_list = []
+    
+    # sorted() : permet de forcer le bon ordre du chargement des frames d'animation (ex: 0.png, 1.png...)
+    for file_name in sorted(os.listdir(path)):
+        full_path = os.path.join(path, file_name)
+        
+        # Vérifie que ce soit un fichier (pas un dossier)
+        if os.path.isfile(full_path):
+            try:
+                image_surf = pygame.image.load(full_path).convert_alpha()
+                surface_list.append(image_surf)
+            except pygame.error:
+                # Ignore les fichiers qui ne sont pas des images
+                pass
+                
+    return surface_list
